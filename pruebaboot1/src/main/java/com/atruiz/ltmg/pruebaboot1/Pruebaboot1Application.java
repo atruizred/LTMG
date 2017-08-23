d88bd3c7dd8e54e02cefb0c246e60df3b8bb896f
atruizred <a.ruiz.r@hotmail.es>
2017-08-23 11:54:14 +0200

    ADD
    Primer commit

---
+++ pruebaboot1/src/main/java/com/atruiz/ltmg/pruebaboot1/Pruebaboot1Application.java
@@ -0,0 +1,12 @@
+package com.atruiz.ltmg.pruebaboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pruebaboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pruebaboot1Application.class, args);
+	}
+}
---
+++ pruebaboot1/src/test/java/com/atruiz/ltmg/pruebaboot1/Pruebaboot1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atruiz.ltmg.pruebaboot1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pruebaboot1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
